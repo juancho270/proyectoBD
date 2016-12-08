3b37751ec42745a7b54b661e9000ec2fe757659e
Juancho270 <Juancho270@Juancho>
2016-12-08 10:00:14 -0500

    Se adhiere la interfaz

---
+++ src/GUI/Implementacion.java
@@ -0,0 +1,32 @@
+package GUI;
+
+
+import java.io.IOException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author usuario-dintev
+ */
+public class Implementacion {
+    Interfaz interfaz;
+    public Implementacion(){
+        interfaz= new Interfaz(this);        
+            
+        
+    }
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+         Implementacion obj=new Implementacion();
+        // TODO code application logic her
+    }
+
+//otro cambio de prueba   
+}
